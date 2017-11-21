3e7f30797ded03f6fc0424749ad2beb7b5d52395
Neal <nealvis@gmail.com>
2017-11-21 13:38:14 -0800

    initial commit for stream_ty_gn_threaded

---
+++ apps/stream_ty_gn_threaded/camera_processor.py
@@ -0,0 +1,88 @@
+#! /usr/bin/env python3
+
+# Copyright(c) 2017 Intel Corporation.
+# License: MIT See LICENSE file in root directory.
+# NPS
+
+# pulls images from camera device and places them in a Queue
+
+#import numpy as np
+import cv2
+import queue
+import threading
+
+class camera_processor:
+
+    # Requested and actual camera dimensions
+    REQUEST_CAMERA_WIDTH = 640  # TY_NETWORK_IMAGE_WIDTH
+    REQUEST_CAMERA_HEIGHT = 480  # TY_NETWORK_IMAGE_HEIGHT
+
+    # Specifies which camera to use.  If only one it will likely be index 0
+    CAMERA_INDEX = 0
+
+    def __init__(self, output_queue):
+        self._camera_device = cv2.VideoCapture(camera_processor.CAMERA_INDEX)
+
+        if ((self._camera_device == None) or (not self._camera_device.isOpened())):
+            print('\n\n')
+            print('Error - could not open camera.  Make sure it is plugged in.')
+            print('Also, if you installed python opencv via pip or pip3 you')
+            print('need to uninstall it and install from source with -D WITH_V4L=ON')
+            print('Use the provided script: install-opencv-from_source.sh')
+            print('\n\n')
+            return
+
+        self._camera_device.set(cv2.CAP_PROP_FRAME_WIDTH, camera_processor.REQUEST_CAMERA_WIDTH)
+        self._camera_device.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_processor.REQUEST_CAMERA_HEIGHT)
+
+        self._actual_camera_width = self._camera_device.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self._actual_camera_height = self._camera_device.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        print('actual camera resolution: ' + str(self._actual_camera_width) + ' x ' + str(self._actual_camera_height))
+
+        self._output_queue = output_queue
+        self._worker_thread = threading.Thread(target=self.do_work, args=())
+
+
+    def get_actual_camera_width(self):
+        return self._actual_camera_width
+
+    def get_actual_camera_height(self):
+        return self._actual_camera_height
+
+
+    def start_processing(self):
+        self._end_flag = False
+        self._worker_thread.start()
+
+    def stop_processing(self):
+        self._end_flag = True
+        # remove one item off queue to allow the current put to finish
+        # if the worker thread is blocked waiting to put
+        try:
+            input_image = self._output_queue.get(False)
+            self._output_queue.task_done()
+        except:
+            print('handling exception')
+            pass
+        self._worker_thread.join()
+
+    def do_work(self):
+        print('in camera_processor worker thread')
+        if (self._camera_device == None):
+            print('camera_processor camera_device is None, returning.')
+            return
+
+        while (not self._end_flag):
+            ret_val, input_image = self._camera_device.read()
+            if (not ret_val):
+                print("No image from camera, exiting")
+                break
+            print('camera_processor got image')
+            self._output_queue.put(input_image, True, 4)
+            print('camera_processor queued image')
+
+        print('exiting camera_processor worker thread')
+
+    def cleanup(self):
+        # close camera
+        self._camera_device.release()
---
+++ apps/stream_ty_gn_threaded/googlenet_processor.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python3
+
+# Copyright(c) 2017 Intel Corporation.
+# License: MIT See LICENSE file in root directory.
+# NPS
+
+# processes images via googlenet
+
+from mvnc import mvncapi as mvnc
+import numpy as np
+import cv2
+import queue
+
+class queue_item:
+    def __init__(self, input_image, user_obj):
+        self.list = (input_image, user_obj)
+
+class googlenet_processor:
+    # GoogLeNet assumes input images are these dimensions
+    GN_NETWORK_IMAGE_WIDTH = 224
+    GN_NETWORK_IMAGE_HEIGHT = 224
+
+    EXAMPLES_BASE_DIR = '../../'
+    ILSVRC_2012_dir = EXAMPLES_BASE_DIR + 'data/ilsvrc12/'
+
+    MEAN_FILE_NAME = ILSVRC_2012_dir + 'ilsvrc_2012_mean.npy'
+
+    LABELS_FILE_NAME = ILSVRC_2012_dir + 'synset_words.txt'
+
+    def __init__(self, googlenet_graph_file, ncs_device):
+
+        # GoogLenet initialization
+
+        # googlenet mean values will be read in from .npy file
+        self._gn_mean = [0., 0., 0.]
+
+        # labels to display along with boxes if googlenet classification is good
+        # these will be read in from the synset_words.txt file for ilsvrc12
+        self._gn_labels = [""]
+
+        # loading the means from file
+        try:
+            self._gn_mean = np.load(googlenet_processor.MEAN_FILE_NAME).mean(1).mean(1)
+        except:
+            print('\n\n')
+            print('Error - could not load means from ' + googlenet_processor.MEAN_FILE_NAME)
+            print('\n\n')
+            raise
+
+        # loading the labels from file
+        try:
+            self._gn_labels = np.loadtxt(googlenet_processor.LABELS_FILE_NAME, str, delimiter='\t')
+            for label_index in range(0, len(self._gn_labels)):
+                temp = self._gn_labels[label_index].split(',')[0].split(' ', 1)[1]
+                self._gn_labels[label_index] = temp
+        except:
+            print('\n\n')
+            print('Error - could not read labels from: ' + googlenet_processor.LABELS_FILE_NAME)
+            print('\n\n')
+            raise
+
+        # Load googlenet graph from disk and allocate graph via API
+        try:
+            with open(googlenet_graph_file, mode='rb') as gn_file:
+                gn_graph_from_disk = gn_file.read()
+            self._gn_graph = ncs_device.AllocateGraph(gn_graph_from_disk)
+
+        except:
+            print('\n\n')
+            print('Error - could not load googlenet graph file: ' + googlenet_graph_file)
+            print('\n\n')
+            raise
+
+        self._input_queue = queue.Queue()
+
+
+    def cleanup(self):
+        self._gn_graph.DeallocateGraph()
+
+
+    def queue_inference(self, input_image, user_obj):
+        self._input_queue.put_nowait(queue_item(input_image, user_obj))
+
+    def dequeque_result(self):
+        work_item = self._input_queue.get(True, 5)
+        index, label, probability = self.googlenet_inference(work_item[0], work_item[1])
+        self._input_queue.task_done()
+        return index, label, probability
+
+    # Executes an inference using the googlenet graph and image passed
+    # gn_graph is the googlenet graph object to use for the inference
+    #   its assumed that this has been created with allocate graph and the
+    #   googlenet graph file on an open NCS device.
+    # input_image is the image on which a googlenet inference should be
+    #   executed.  It will be resized to match googlenet image size requirements
+    #   and also converted to float32.
+    # returns a list of the following three items
+    #   index of the most likely classification from the inference.
+    #   label for the most likely classification from the inference.
+    #   probability the most likely classification from the inference.
+    def googlenet_inference(self, input_image, user_obj):
+
+        # Resize image to googlenet network width and height
+        # then convert to float32, normalize (divide by 255),
+        # and finally convert to convert to float16 to pass to LoadTensor as input for an inference
+        input_image = cv2.resize(input_image, (googlenet_processor.GN_NETWORK_IMAGE_WIDTH,
+                                               googlenet_processor.GN_NETWORK_IMAGE_HEIGHT),
+                                 cv2.INTER_LINEAR)
+
+        input_image = input_image.astype(np.float32)
+        input_image[:, :, 0] = (input_image[:, :, 0] - self._gn_mean[0])
+        input_image[:, :, 1] = (input_image[:, :, 1] - self._gn_mean[1])
+        input_image[:, :, 2] = (input_image[:, :, 2] - self._gn_mean[2])
+
+        # Load tensor and get result.  This executes the inference on the NCS
+        self._gn_graph.LoadTensor(input_image.astype(np.float16), user_obj)
+        output, userobj = self._gn_graph.GetResult()
+
+        order = output.argsort()[::-1][:1]
+
+        '''
+        print('\n------- prediction --------')
+        for i in range(0, 5):
+            print('prediction ' + str(i) + ' (probability ' + str(output[order[i]]) + ') is ' + self._gn_labels[
+                order[i]] + '  label index is: ' + str(order[i]))
+        '''
+
+        # index, label, probability
+        return order[0], self._gn_labels[order[0]], output[order[0]]
