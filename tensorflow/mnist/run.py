c717151f6b87794725d1cdcb6fa2f1dd01747cdf
Neal <neal.p.smith@intel.com>
2018-03-16 14:29:32 -0700

    run.py will read an image and classify it now with proper preprocessing

--- tensorflow/mnist/run.py
+++ tensorflow/mnist/run.py
@@ -15,22 +15,18 @@ import sys
 import re
 from os import system
 
-dim=(224,224)
-EXAMPLES_BASE_DIR='../../'
-imagename = EXAMPLES_BASE_DIR+'data/images/nps_electric_guitar.png'
+dim=(28,28)
+
+imagename = '/home/neal/Downloads/mnist/testSample/img_1.jpg'
 
 def infer(imgname):
         # ***************************************************************
         # get labels
         # ***************************************************************
-        labels_file=EXAMPLES_BASE_DIR+'data/ilsvrc12/synset_words.txt'
-        labels=numpy.loadtxt(labels_file,str,delimiter='\t')
+
+        labels=[ 'digit 0', 'digit 1', 'digit 2', 'digit 3', 'digit 4', 'digit 5', 'digit 6', 'digit 7', 'digit 8', 'digit 9']
         
-        # ***************************************************************
-        # configure the NCS
-        # ***************************************************************
-        mvnc.SetGlobalOption(mvnc.GlobalOption.LOG_LEVEL, 2)
-        
+
         # ***************************************************************
         # Get a list of ALL the sticks that are plugged in
         # ***************************************************************
@@ -50,9 +46,8 @@ def infer(imgname):
         device.OpenDevice()
 
         filefolder = os.path.dirname(os.path.realpath(__file__)) 
-        network_blob = filefolder + '/graph'
-        system('(cd ' + filefolder + ';test -f graph || make compile)')
-        
+        network_blob = filefolder + '/mnist_inference.graph'
+
         #Load blob
         with open(network_blob, mode='rb') as f:
                 blob = f.read()
@@ -62,13 +57,16 @@ def infer(imgname):
         # ***************************************************************
         # Load the image
         # ***************************************************************
-        ilsvrc_mean = numpy.load(EXAMPLES_BASE_DIR+'data/ilsvrc12/ilsvrc_2012_mean.npy').mean(1).mean(1) #loading the mean file
+        mnist_mean = [128.0, 128.0, 128.0]
         img = cv2.imread(imgname)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img=cv2.resize(img,dim)
+
         img = img.astype(numpy.float32)
-        img[:,:,0] = (img[:,:,0] - ilsvrc_mean[0])
-        img[:,:,1] = (img[:,:,1] - ilsvrc_mean[1])
-        img[:,:,2] = (img[:,:,2] - ilsvrc_mean[2])
+        img[:] = ((img[:] - 128.0)*(1.0/255.0))
+        #img[:,:,0] = ((img[:,:,0] - mnist_mean[0])*(1/255.0))
+        #img[:,:,1] = ((img[:,:,1] - mnist_mean[1])*(1/255.0))
+        #img[:,:,2] = ((img[:,:,2] - mnist_mean[2])*(1/255.0))
         
         # ***************************************************************
         # Send the image to the NCS
@@ -79,17 +77,39 @@ def infer(imgname):
         # Get the result from the NCS
         # ***************************************************************
         output, userobj = graph.GetResult()
-        
+
+        print ('-----------------------------------------------------------')
+        print ('file: ' + imgname)
+        print ('-----------------------------------------------------------')
+        res_str = 'Raw inference results: ['
+        for out_index in range(0, len(output) ):
+            res_str = res_str + str(output[out_index])
+            if (out_index != (len(output)-1)):
+                res_str = res_str + ', '
+        res_str = res_str + ']'
+        print (res_str)
+
+        print ('---')
+        print ('Top 5 results from most ceretain to least:')
+        print ('---')
+        result = ''
+        five_highest_indices = (-output).argsort()[:5]
+        for index in range(0, 5):
+            one_prediction = 'certainty ' + str(output[five_highest_indices[index]]) + ' --> ' + labels[five_highest_indices[index]]
+            #print (one_prediction)
+            result = result + one_prediction + '\n'
+
+
         # ***************************************************************
         # Print the results of the inference form the NCS
         # ***************************************************************
-        order = output.argsort()[::-1][:6]
-        print('\n------- predictions --------')
-        result = ""
-        for i in range(0,5):
-                #print ('prediction ' + str(i) + ' (probability ' + str(output[order[i]]) + ') is ' + labels[order[i]] + '  label index is: ' + str(order[i]) )
-                label = re.search("n[0-9]+\s([^,]+)", labels[order[i]]).groups(1)[0]
-                result = result + "\n%20s %0.2f %%" % (label, output[order[i]]*100)
+        #order = output.argsort()[::-1][:6]
+        #print('\n------- predictions --------')
+        #result = ""
+        #for i in range(0,5):
+        #        #print ('prediction ' + str(i) + ' (probability ' + str(output[order[i]]) + ') is ' + labels[order[i]] + '  label index is: ' + str(order[i]) )
+        #        label = re.search("n[0-9]+\s([^,]+)", labels[order[i]]).groups(1)[0]
+        #        result = result + "\n%20s %0.2f %%" % (label, output[order[i]]*100)
         
         # ***************************************************************
         # Clean up the graph and the device
@@ -102,5 +122,7 @@ def infer(imgname):
 if __name__ == "__main__":
         result,imgname = infer(imagename)
         print (result)
+        print ('-----------------------------------------------------------')
+
 
 
