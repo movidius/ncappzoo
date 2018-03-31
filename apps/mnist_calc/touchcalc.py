ed15ac5b8dd3f1944b705b3f2a40eab93d9a2e88
Neal <neal.p.smith@intel.com>
2018-03-31 12:21:10 -0700

    extacting op1 and op2 and drawing struture on the canvas
    Signed-off-by: Neal <neal.p.smith@intel.com>

--- apps/mnist_calc/touchcalc.py
+++ apps/mnist_calc/touchcalc.py
@@ -5,7 +5,7 @@ import digitdetector
 
 
 class TouchCalc:
-    def __init__(self, window_title='TouchCalc', width=800, height=800):
+    def __init__(self, window_title='TouchCalc', width=1400, height=500):
         # Save these for use later
         self._height = height
         self._width = width
@@ -24,8 +24,37 @@ class TouchCalc:
         self._menu_bar_threshold = int(self._height * .9)
 
         # Set up a blank canvas
-        self._canvas = np.zeros((width, height, 3), np.uint8)
+        self._canvas = np.zeros((height, width, 3), np.uint8)
+
+        self._box_color = (230, 230, 230)
+
+        self._operation_width = 80
+        self._operation_height = 80
+        self._pad = 10
+
+        self._operand1_left = 20
+        self._operand1_top = 50
+        self._operand1_width = 400
+        self._operand1_height = 350
+
+        self._operand2_left = self._operand1_left + self._operand1_width + self._operation_width + self._pad + self._pad
+        self._operand2_top = self._operand1_top
+        self._operand2_width = self._operand1_width
+        self._operand2_height = self._operand1_height
+
+        # operation rectangle, +, -, *, / etc
+        self._operation_color = (0, 0, 0)
+        self._operation_left = self._operand1_left + self._operand1_width + self._pad
+        self._operation_top = self._operand1_top + (int)(self._operand1_height / 2) - (int)(self._operation_height / 2)
+
+        # equals sign
+        self._equals_height = self._operation_height
+        self._equals_width = self._operation_width
+        self._equals_left = self._operand2_left + self._operand2_width + self._pad
+        self._equals_top = self._operand2_top + (int)(self._operand2_height / 2) - (int)(self._equals_height / 2)
+
         self._clear()
+
 
     def _clear(self):
         """Clear the canvas and redraw buttons."""
@@ -61,6 +90,35 @@ class TouchCalc:
 
         cv2.putText(self._canvas, 'C', (0, y_coord), font_name, font_scale, color, font_thickness)
         cv2.putText(self._canvas, '=', (self._width - 50, y_coord), font_name, font_scale, color, font_thickness)
+
+        #operand 1 rect
+        cv2.rectangle(self._canvas, (self._operand1_left, self._operand1_top),
+                      (self._operand1_left + self._operand1_width, self._operand1_top+self._operand1_height),
+                      self._box_color, 1)
+
+        #operand 2 rect
+        cv2.rectangle(self._canvas, (self._operand2_left, self._operand2_top),
+                      (self._operand2_left + self._operand2_width, self._operand2_top+self._operand2_height),
+                      self._box_color, 1)
+
+        # operation ( + )
+        #cv2.rectangle(self._canvas, (operation_left, operation_top), (operation_left + operation_width, operation_top+operation_height), box_color, 1)
+        cv2.line(self._canvas, (self._operation_left + (int)(self._operation_width/2), self._operation_top ),
+                 (self._operation_left + (int)(self._operation_width / 2), self._operation_top + self._operation_height),
+                 self._operation_color, 5)
+        cv2.line(self._canvas, (self._operation_left, self._operation_top + (int)(self._operation_height/2) ),
+                 (self._operation_left+self._operation_width, self._operation_top + (int)(self._operation_height / 2)),
+                 self._operation_color, 5)
+
+
+        #cv2.rectangle(self._canvas, (equals_left, equals_top), (equals_left + equals_width, equals_top+equals_height), box_color, 1)
+        cv2.line(self._canvas, (self._equals_left, self._equals_top + (int)(self._equals_height/2) - 10 ),
+                 (self._equals_left+self._equals_width, self._equals_top + (int)(self._equals_height / 2) - 10),
+                 self._operation_color, 5)
+        cv2.line(self._canvas, (self._equals_left, self._equals_top + (int)(self._equals_height/2) + 10),
+                 (self._equals_left+self._equals_width, self._equals_top + (int)(self._equals_height / 2)+ 10),
+                 self._operation_color, 5)
+
 
     def close(self):
         """Close and destroy the window."""
@@ -107,6 +165,16 @@ class TouchCalc:
 
         # Detect the digits
         digits = digitdetector.detect(self._canvas)
+        for box in digits :
+            print(box)
+
+        box = digits[0]
+        print(box)
+
+        op1_image = self._canvas[self._operand1_top : self._operand1_top + self._operand1_height,  self._operand1_left : self._operand1_left + self._operand1_width]
+        op2_image = self._canvas[self._operand2_top : self._operand2_top + self._operand2_height,  self._operand2_left : self._operand2_left + self._operand2_width]
+        cv2.imshow("op1", op1_image)
+        cv2.imshow("op2", op2_image)
 
         # Draw the buttons again
         self._draw_ui()
